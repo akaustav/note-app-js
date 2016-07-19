5c2823e80692917b6807a5ffadce76518d0adeb4
Ameet Kaustav <ameet.kaustav@gmail.com>
2016-07-18 23:55:17 -0400

    Refactored HTML, CSS and JS
    Placed all codes into the window.onload

--- logic.js
+++ logic.js
@@ -1,24 +1,35 @@
-var arr = [];
+window.onload = function () {
+  var arr = [];
 
-function addNote() {
+  var addBtn = document.getElementsByClassName('add')[0];
 
-  var note = document.getElementById('note').value;
-  arr.push(note);
-  var index = arr.length - 1;
+  addBtn.addEventListener('click', addNote, false);
+
+  /* Event Handlers */
+  function addNote(event) {
+    event.preventDefault();
+
+    var note = document.getElementById('note').value;
+    arr.push(note);
+    var index = arr.length - 1;
     var text = arr[index];
+
     var newElement = document.createElement('div');
     newElement.className = "addedText";
     newElement.id = "text" + index;
     var newContent = document.createTextNode(text);
+
     newElement.appendChild(newContent);
+
     var checkbox = document.createElement('input');
     checkbox.type = 'radio';
     checkbox.id = "checkbox" + index;
     checkbox.className = 'status'
+
     newElement.appendChild(checkbox);
-    
+
     var currentElement = document.getElementById('#node');
     document.body.insertBefore(newElement, currentElement); 
     document.getElementById('note').value = '';
-}
-
+  }
+};
